3ae657340ff3df7accfd2214e76d36b8b78cb21c
Borges Tech <105560949+nenenborges@users.noreply.github.com>
2024-06-16 09:41:52 -0300

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,3 +1,8 @@
+//teste de conexao
+import { MercadoPagoConfig, Preference } from "https://sdk.mercadopago.com/js/v2"
+//const client = new MercadoPagoConfig({accessToken:"TEST-3212641412207539-060606-7cdd9e1aa6602db6c5536087d189cbcb-1810191826"});
+
+
 const mp = new MercadoPago("TEST-b2c96b64-a20b-4bdf-958a-a583fdbeb64b", {
   locale: 'pt-BR', // 'pt-BR'
 });
@@ -5,7 +10,8 @@ const mp = new MercadoPago("TEST-b2c96b64-a20b-4bdf-958a-a583fdbeb64b", {
 
 document.getElementById("checkout-btn").addEventListener("click", async () => {
   try{
-      const orderData = {
+    
+   /*   const orderData = {
           title: "MATERIAL DE INFORMÁTICA",//document.querySelector(".name").innerTest,
           quantity: 1,
           price: document.getElementsByClassName("total-price")[0].innerText,
@@ -23,7 +29,32 @@ document.getElementById("checkout-btn").addEventListener("click", async () => {
       const preference = await response.json();
       createCheckoutButton(preference.id);
   }   catch(error) {
-      alert("error:(");  
+      alert("error:(");  */
+
+    //---------------------===
+    // Cria um objeto de preferência
+PreferenceClient client = new PreferenceClient();
+
+// Cria um item na preferência
+PreferenceItemRequest item =
+   PreferenceItemRequest.builder()
+       .title("Meu produto")
+       .quantity(1)
+       .unitPrice(new BigDecimal("75"))
+       .build();
+
+MercadoPagoConfig.setAccessToken("TEST-3212641412207539-060606-7cdd9e1aa6602db6c5536087d189cbcb-1810191826");
+
+List<PreferenceItemRequest> items = new ArrayList<>();
+items.add(item);
+
+PreferenceRequest request =
+   PreferenceRequest.builder().items(items).purpose("wallet_purchase").build();
+
+client.create(request);
+
+
+    //-----------------------///
   } 
 });
 
